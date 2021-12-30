07245d8b926593d3d3ee1dbca69dd4628274edc6
Chupriarti <wallx13@gmail.com>
2021-12-30 22:46:04 +0300

    add webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -6,5 +8,9 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].[hash].js"
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({template: "./src/index.html"}),
+        new CleanWebpackPlugin()
+    ]
 }
