96a9dc69375d2c1d4b23ca6db3592967247d3a5d
Chupriarti <wallx13@gmail.com>
2022-01-01 12:35:42 +0300

    prepare reposReducer

---
+++ src/reducers/reposReducer..js
@@ -0,0 +1,11 @@
+const defaultState ={
+    items: [],
+    iFetching: true
+}
+
+export default function reposReducer(state = defaultState, action){
+    switch (action.type) {
+        default: 
+            return state;
+    }
+}
