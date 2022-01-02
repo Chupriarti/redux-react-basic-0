0894aba1261cac5a6d3ba05797376bb13768c7c2
Chupriarti <wallx13@gmail.com>
2022-01-02 23:07:06 +0300

    use getRepos in Main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRepos } from '../actions/repos';
 import './main.less'
 
 const Main = () => {
+    const dispatch = useDispatch();
+    const repos = useSelector(state => state.repos.items);
+
+    React.useEffect(() => {
+        dispatch(getRepos());
+    }, [])
+
     return (
         <div className="main">
             Main
