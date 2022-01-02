bd1e5a7dae2ebece3da7f4b804198fc5714f42cf
Chupriarti <wallx13@gmail.com>
2022-01-02 23:01:10 +0300

    prepare component for repo

---
+++ src/components/main/repo/Repo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './repo.less'
+
+const Repo = (props) => {
+    const repo = props.repo;
+
+    return (
+        <div className="repo">
+            Repo
+        </div>
+    )
+}
+
+export default Repo;
