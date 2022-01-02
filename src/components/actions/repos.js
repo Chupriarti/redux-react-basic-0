be379546e23e71c14c811d1991de812c334b8606
Chupriarti <wallx13@gmail.com>
2022-01-02 23:12:38 +0300

    fix

--- src/components/actions/repos.js
+++ src/components/actions/repos.js
@@ -3,7 +3,7 @@ import {setRepos} from "../../reducers/reposReducer"
 
 export const getRepos = (searchQuesry = "stars:%3E1") => {
     return async (dispatch) => {
-        const response = axios.get(`https://api.github.com/search/repositories?q=${searchQuesry}&sort=stars`)
+        const response = await axios.get(`https://api.github.com/search/repositories?q=${searchQuesry}&sort=stars`)
         dispatch(setRepos(response.data));
     }
 }
