27ba3d3081a55073ecb5944739cef7c1c6e8a93a
Chupriarti <wallx13@gmail.com>
2022-01-02 19:52:18 +0300

    remove temporary code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setCount } from '../reducers/reposReducer.';
 import './app.less'
 
 const App = () => {
-    const dispatch = useDispatch();
-    const count = useSelector(state => state.repos.count);
 
-    function onCountClick(){
-        dispatch(setCount(5));
-    }
 
     return (
         <div className="app">
-            <button onClick={onCountClick}>COUNT</button>
-            <div>{count}</div>
         </div>
     )
 }
--- src/reducers/reposReducer..js
+++ src/reducers/reposReducer..js
@@ -2,20 +2,12 @@ const SET_COUNT = "SET_COUNT";
 
 const defaultState ={
     items: [],
-    iFetching: true,
-    count: 0
+    iFetching: true
 }
 
 export default function reposReducer(state = defaultState, action){
     switch (action.type) {
-        case SET_COUNT:
-            return {
-                ...state,
-                count: action.payload
-            }
         default: 
             return state;
     }
 }
-
-export const setCount = (count) => ({type: SET_COUNT, payload: count})
