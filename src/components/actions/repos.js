02e3eeca513eac8340508c09bc2a47d1b6bdc41a
Chupriarti <wallx13@gmail.com>
2022-01-03 17:02:48 +0300

    add fix if serach input is empty

--- src/components/actions/repos.js
+++ src/components/actions/repos.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import {setIsFetching, setRepos} from "../../reducers/reposReducer"
 
 export const getRepos = (searchQuesry = "stars:%3E1") => {
+    if (searchQuesry === ""){
+        searchQuesry = "stars:%3E1";
+    }
     return async (dispatch) => {
         dispatch(setIsFetching(true));
         const response = await axios.get(`https://api.github.com/search/repositories?q=${searchQuesry}&sort=stars`)
