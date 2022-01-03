0703e4fc57d340b9f97bc76b99022dbbcaac5cea
Chupriarti <wallx13@gmail.com>
2022-01-03 16:34:49 +0300

    add isFetching to Main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -7,6 +7,7 @@ import Repo from './repo/Repo';
 const Main = () => {
     const dispatch = useDispatch();
     const repos = useSelector(state => state.repos.items);
+    const isFetching = useSelector(state => state.repos.isFetching);
 
     React.useEffect(() => {
         dispatch(getRepos());
