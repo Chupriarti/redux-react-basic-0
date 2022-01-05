df85aaebc69e4ef0f1f1170c480092c09cfbc344
Chupriarti <wallx13@gmail.com>
2022-01-05 12:25:46 +0300

    prepare repo Card page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './app.less'
+import Card from './card/Card';
 import Main from './main/Main';
 
 const App = () => {
@@ -9,7 +10,8 @@ const App = () => {
         <BrowserRouter>
             <div className='container'>
                 <Routes>
-                    <Route path="/" element={<Main />} />
+                    <Route path="/" element={<Main />} exact />
+                    <Route path="/card" element={<Card />} />
                 </Routes> 
             </div>
         </BrowserRouter>
