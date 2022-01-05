16741d516096500301f50c283d8c91f72eb7c8c8
Chupriarti <wallx13@gmail.com>
2022-01-05 12:32:18 +0300

    add back button to Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Card = () => {
+const Card = (props) => {
+    const navigate = useNavigate();
     return (
         <div>
+            <button onClick={() => navigate(-1)}>BACK</button>
             Card repo
         </div>
     )
