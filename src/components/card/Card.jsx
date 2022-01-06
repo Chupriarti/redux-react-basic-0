e46d6441cdd97e1c7b946e58b1abf7dc9e9685d5
Chupriarti <wallx13@gmail.com>
2022-01-06 21:25:11 +0300

    add current repos show in repo Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getCurrentRepo } from '../actions/repos';
 
 const Card = (props) => {
+    const {username, reponame} = useParams();
+    const [repo, setRepo] = React.useState({});
     const navigate = useNavigate();
+
+    React.useEffect(() => {
+        getCurrentRepo(username, reponame, setRepo);
+    }, [])
+
     return (
         <div>
             <button onClick={() => navigate(-1)}>BACK</button>
-            Card repo
+            <div className="card">
+                <img src="" alt="" />
+                <div className="name">{repo.name}</div>
+                <div className="stars">{repo.stargazers_count}</div>
+            </div>
         </div>
     )
 }
